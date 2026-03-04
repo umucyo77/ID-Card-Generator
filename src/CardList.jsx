e45885702d46a95cc1b517c52a091756114bde56
umucyo77 <eumucyuvakumana77@gmail.com>
2026-03-04 23:00:55 +0200

    Define your Data

--- src/CardList.jsx
+++ src/CardList.jsx
@@ -9,6 +9,35 @@ import IdCard from './components/IdCard'
 function CardList() {
   const [count, setCount] = useState(0)
 
+  const people = [
+    {
+      id: 1,
+      name:"Umucyo Esther",
+      role:"Software Engineer",
+      department:"Engineering",
+      image:"./assets/esther.png"
+    },
+
+    {
+      id: 2,
+      name:"Alice Johnson",
+      role:"Frontend Developer",
+      department:"Engineering",
+      image:"./assets/alice.png"
+
+    },
+
+    {
+      id: 3,
+      name:"Igiraneza Tito",
+      role:"Full Stack Developer",
+      department:"Engineering",
+      image:"./assets/tito.png"
+    }
+
+
+  ]
+
   return (
     <>
     <IdCard />
